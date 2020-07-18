2c7972d11e4258e8b4b8e34ab9f16f8931e0e8a1
AilenGomez <ailenadrianagomez@gmail.com>
2020-07-18 15:35:50 -0300

    adfgfd

--- HeladeriaYCafeteria/src/vistas/reporteTableModel.java
+++ HeladeriaYCafeteria/src/vistas/reporteTableModel.java
@@ -1,5 +1,36 @@
 package vistas;
 
-public class reporteTableModel {
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+public class reporteTableModel extends AbstractTableModel {
+	ArrayList<String> Registros;
+	String[] columnas = {"Codigo", "Nombre","Cantidad","Precio","Subtotal"};
+	Class[] clases = {Integer.class,String.class, Integer.class, Double.class, Double.class};
+
+	public reporteTableModel(ArrayList<String> datos) {
+		this.Registros=datos;
+		
+	}
+	@Override
+	public int getColumnCount() {
+		
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		
+		return null;
+	}
+	
+	
 
 }
